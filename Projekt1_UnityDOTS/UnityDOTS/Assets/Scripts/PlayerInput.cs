f7068047288fcf75a471f28e7b2f8386789ca75c
stefan.friesen <stefan.friesen@informatik.hs-fulda.de>
2023-10-31 11:52:19 +0100

    randomize Rocket anim

--- Projekt1_UnityDOTS/UnityDOTS/Assets/Scripts/PlayerInput.cs
+++ Projekt1_UnityDOTS/UnityDOTS/Assets/Scripts/PlayerInput.cs
@@ -6,31 +6,45 @@ public class PlayerInput : MonoBehaviour
 {
     [SerializeField] Transform spaceshipT;
     [SerializeField] LineRenderer spaceshipLR;
+    float speed, rocketLength;
+    int rocketTimer;
+
+    void Start()
+    {
+        speed = 0.005f;
+        rocketLength = 0.2f;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        rocketTimer++;
+        if (rocketTimer > 40) {
+            rocketLength = Random.Range(0.1f, 0.25f);
+            rocketTimer = 0;
+        }
+
         if (Input.GetKey(KeyCode.W) && spaceshipT.position.y <= 0.75f)
         {
-            spaceshipT.position += new Vector3(0, 0.005f, 0);
+            spaceshipT.position += new Vector3(0, speed, 0);
         }
 
         if (Input.GetKey(KeyCode.S) && spaceshipT.position.y >= -0.35f)
         {
-            spaceshipT.position -= new Vector3(0, 0.005f, 0);
+            spaceshipT.position -= new Vector3(0, speed, 0);
         }
 
         if (Input.GetKey(KeyCode.A) && spaceshipT.position.x <= 1.0f)
         {
-            spaceshipT.position += new Vector3(0.005f, 0, 0);
+            spaceshipT.position += new Vector3(speed, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.D) && spaceshipT.position.x >= -1.0f)
         {
-            spaceshipT.position -= new Vector3(0.005f, 0, 0);
+            spaceshipT.position -= new Vector3(speed, 0, 0);
         }
 
         spaceshipLR.SetPosition(0, spaceshipT.position);
-        spaceshipLR.SetPosition(1, spaceshipT.position + new Vector3(0, 0, 0.2f));
+        spaceshipLR.SetPosition(1, spaceshipT.position + new Vector3(0, 0, rocketLength));
     }
 }
