f05536eb69bd16c0e7d23602a535322acc93e42f
stefan.friesen <stefan.friesen@informatik.hs-fulda.de>
2024-01-09 10:25:07 +0100

    UI aus der VL

---
+++ Projekt3_Godot/Scripts/hostButton.gd
@@ -0,0 +1,18 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_down():
+	text = "Test"
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://TowerDefense.tscn")
+	pass # Replace with function body.
